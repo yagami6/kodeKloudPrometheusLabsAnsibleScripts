524ee103f3272e6bf5e792a6bb2634a660e0299b
yagami6 <sushant88patil@gmail.com>
2024-04-07 16:50:40 +0530

    fixed bug

--- Lab2Authentication/install.sh
+++ Lab2Authentication/install.sh
@@ -1,4 +1,3 @@
-echo hello from $(hostname)
 rm  -r /etc/node_exporter
 mkdir /etc/node_exporter/
 touch /etc/node_exporter/config.yml
@@ -9,11 +8,11 @@ sed -i 's:ExecStart=/usr/local/bin/node_exporter:ExecStart=/usr/local/bin/node_e
 apt -qq update
 apt -qq install apache2-utils -y
 password=$(echo "DevOps" | htpasswd -inBC 10 "" | tr -d ':\n'; echo)
-echo "basic_auth_users:\n   prometheus: $password" > /etc/node_exporter/config.yml
+echo "basic_auth_users:\n   prometheus: $password" >> /etc/node_exporter/config.yml
 openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout node_exporter.key -out node_exporter.crt -subj "/C=US/ST=California/L=Oakland/O=MyOrg/CN=localhost" -addext "subjectAltName = DNS:localhost"
 mv node_exporter.crt node_exporter.key /etc/node_exporter/
 chown nodeusr.nodeusr /etc/node_exporter/node_exporter.key
 chown nodeusr.nodeusr /etc/node_exporter/node_exporter.crt
-echo "tls_server_config::\n   cert_file: node_exporter.crt\n"   key_file: node_exporter.key > /etc/node_exporter/config.yml
+echo "tls_server_config:\n   cert_file: node_exporter.crt\n   key_file: node_exporter.key" >> /etc/node_exporter/config.yml
 systemctl daemon-reload
 systemctl restart node_exporter
